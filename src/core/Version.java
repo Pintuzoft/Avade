64a015d4042dce469fb0dfe69e73179f6b905fd4
DreamHealer <dreamhealer@avade.net>
2023-07-29 21:51:31 +0200

    Fixed version

--- src/core/Version.java
+++ src/core/Version.java
@@ -36,7 +36,7 @@ public class Version extends HashNumeric {
         this.generation     = 1;
         this.year           = 23;
         this.month          = 7;
-        this.build          = 8;
+        this.build          = 9;
         this.bahamut        = "bahamut-2.1.5";
     }
     
