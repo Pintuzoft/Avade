c59774732f1e58cfea321da08a6fd68d71f5fe16
DreamHealer <dreamhealer@avade.net>
2023-07-29 18:34:28 +0200

    Fixed version typo

--- src/core/Version.java
+++ src/core/Version.java
@@ -32,8 +32,8 @@ public class Version extends HashNumeric {
     public Version ( )  {
         this.generation     = 1;
         this.year           = 23;
-        this.month          = 9;
-        this.build          = 1;
+        this.month          = 7;
+        this.build          = 2;
         this.bahamut        = "bahamut-2.1.4";
     }
     
